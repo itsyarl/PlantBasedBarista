72acbd3279c2a6a45a5be10487e19151f5b0ac0a
itsyarl <yarl.van.onckelen@gmail.com>
2019-10-24 13:12:44 +0200

    (style and javascript order

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import data from '../src/assets/js/coffees.json';
 
 const init = () => {
   getCoffees(data);
+  getOrder(data);
+  // addItem(data);
 };
 
 // begrensd tot twee getallen na comma
@@ -34,6 +36,56 @@ const makeButton = data => {
                       <span class="price__button__plus">+</span>
                     </a>`;
   $prijsLijst.appendChild($li);
+};
+
+const getOrder = () => {
+  const $items = document.querySelectorAll(`.price`);
+  $items.forEach(function($item) {
+    $item.addEventListener(`click`, e => {
+      addItem(e.composedPath()[2]);
+    });
+  });
+};
+
+const addItem = e => {
+  const num = e.getAttribute('data_id');
+  console.log(num);
+  const naam = document.querySelector(`.price:nth-child(${num}n) .price__button__name`).innerHTML;
+  console.log(naam);
+  const prijs = document.querySelector(`.price:nth-child(${num}n) .price__button__amount`).innerHTML;
+  console.log(prijs);
+  const $order = document.querySelector(`.orders`);
+  // attribuut word genomen maar heb de naam en prijs er van nodig
+  const $li = document.createElement(`li`);
+  $li.classList.add(`order`);
+  $li.innerHTML = `<span class="order__name">
+                    <span class="order__amount">1x</span> ${naam}
+                  </span>
+                  <div class="order__wrapper">
+                    <span class="order__price">${prijs}</span>
+                    <button class="remove">
+                      x
+                    </button>
+                  </div>`;
+
+  $order.appendChild($li);
+  const $item = document.querySelector(`.order`);
+  const $list = document.querySelector(`.orders`).contains($item);
+  toggleContent($list);
+};
+
+const toggleContent = empty => {
+  const $empty = document.querySelector(`.emptystate`);
+  const $notEmpty = document.querySelector(`.orders__wrapper`);
+  console.log(empty);
+  if (empty === true) {
+    $empty.classList.add(`hide`);
+    $notEmpty.classList.remove(`hide`);
+  }
+  if (empty === false) {
+    $empty.classList.remove(`hide`);
+    $notEmpty.classList.add(`hide`);
+  }
 };
 
 
